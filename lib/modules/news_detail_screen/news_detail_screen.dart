30a71b07ee02bcbcd02cf31cfe393b3b632097cd
alfinramadhana <alfinramadhana07@gmail.com>
2023-03-29 16:53:35 +0700

    first commit

---
+++ lib/modules/news_detail_screen/news_detail_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class NewsDetailScreen extends StatefulWidget {
+  const NewsDetailScreen({
+    super.key,
+    required this.id,
+  });
+  final String id;
+
+  @override
+  State<NewsDetailScreen> createState() => _NewsDetailScreenState();
+}
+
+class _NewsDetailScreenState extends State<NewsDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              "https://i.postimg.cc/y6yh0TqJ/gambar1.jpg",
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                   const SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment:MainAxisAlignment.center ,
+                      children: const [
+                        Text(
+                          'EXO',
+                           style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                  const Text(
+                    'Grup paling The Best in The World',
+                    
+                    maxLines: 10,
+                    textAlign: TextAlign.justify,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                  ],
+                ),
+              ),
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
+
+class NewsDetailScreenHot extends StatefulWidget {
+  const NewsDetailScreenHot({
+    super.key, 
+    required this.newsDetailHot,
+  });
+  final String newsDetailHot;
+
+  @override
+  State<NewsDetailScreenHot> createState() => _NewsDetailScreenHotState();
+}
+
+class _NewsDetailScreenHotState extends State<NewsDetailScreenHot> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              "https://i.postimg.cc/wjmQRY9z/gambar2.jpg",
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                   const SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment:MainAxisAlignment.center ,
+                      children: const [
+                        Text(
+                          'Girls Generation',
+                           style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                  const Text(
+                    'Girl Generation Grup paling Ikonic di Korea dan Dunia',
+                    maxLines: 10,
+                    textAlign: TextAlign.justify,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                  ],
+                ),
+              ),
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
